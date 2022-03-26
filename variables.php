62238e7a2b92b539297d64286970878100a3ab8a
ChitownCoder <chitowncoder@gmail.com>
2022-03-25 23:14:59 -0500

    went over variables a little more by assiging them

--- variables.php
+++ variables.php
@@ -1,3 +1,21 @@
 <?php
 
 $a = 1;
+
+$b = 34.567;
+
+$firstName = 'tony';
+
+$isMemberOfSportsClub = TRUE;
+
+$Color = 'green';
+
+$first_name = 'sam';
+
+$this9 = 'blue'; // should not use a variable like this...
+
+$9this = 'Cannot do this!';
+
+$_name = 'bob'; // do not do this honestly
+
+$this = 'blue'; // never use this key word for a variable, it is used for objects
