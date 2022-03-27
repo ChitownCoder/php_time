6e6f4a178d971b309102163f16b990bab6401b38
ChitownCoder <chitowncoder@gmail.com>
2022-03-27 14:16:59 -0500

    addition and subtraction

---
+++ basic-operators/subtraction.php
@@ -0,0 +1,26 @@
+<?php
+
+//$firstNumber = 5;
+
+//$secondNumber = 10;
+
+//$sum = $firstNumber - $secondNumber;
+
+//echo $sum;
+
+//echo "<br>";
+
+//echo $firstNumber . " - " . $secondNumber . " = " . $sum;
+
+
+$firstNumber = 20;
+
+$secondNumber = 12;
+
+$answer = $firstNumber - $secondNumber;
+
+echo $answer;
+
+echo "<br>";
+
+echo "$firstNumber - $secondNumber = $answer";
