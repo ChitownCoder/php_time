fd30cd5fd408a0379ed56c57583e6986412f721d
ChitownCoder <chitowncoder@gmail.com>
2022-03-26 19:56:28 -0500

    Learning how to use concatenation

---
+++ php-basics/echo.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "hello world";
+
+echo "<br>";
+
+$firstNumber = 5;
+
+echo $firstNumber;
+
+echo "<br>";
+
+$isMember - FALSE;
+
+echo $isMember;
+
+echo "<br>";
+
+//print "blue"; // print or echo is a personal choice, echo is commonly used
