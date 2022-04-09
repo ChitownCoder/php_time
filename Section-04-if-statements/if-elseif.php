c69bc94795174ab5a5b7bb9aca388f1532ef4840
ChitownCoder <chitowncoder@gmail.com>
2022-04-09 15:33:02 -0500

    went over elseif

---
+++ Section-04-if-statements/if-elseif.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 10;
+
+$b = 10;
+
+$c = TRUE;
+
+$d = FALSE;
+
+if ($a > $b) { // if true
+    echo "is greater than"; // do this
+} else if ($a < $b) { // if this is also true
+    echo "is less than"; // do this 
+} else { // if not true 
+    echo "is equal to"; // do this
+}
